3117e36bc2ddc0ad3b6707b648837b284dd7a2a3
DS Hwang <hds2g22@gmail.com>
2020-01-29 07:27:58 +0900

    Creating yirst React Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Potato from "./Potato";
 
 function App() {
-  return <div className="App">Good morning!!</div>;
+  return (
+    <div>
+      <h1>Good morning!!</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function potato() {
+  return <h3>This is potato</h3>;
+}
+
+export default potato;
