edffe0028d6a362fe7ffebae89a0049ca4bfec05
DS Hwang <hds2g22@gmail.com>
2020-01-29 07:49:43 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Potato from "./Potato";
+
+//function Food({ props.favorite }) {
+//function Food( props ) {
+function Food({ favorite }) {
+  return <h1>I like {favorite}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Good morning!!</h1>
-      <Potato />
+      <Food favorite="aaa" />
+      <Food favorite="bbb" />
+      <Food favorite="ccc" />
+      <Food favorite="ddd" />
+      <Food favorite="eee" />
+      <Food favorite="fff" />
     </div>
   );
 }
