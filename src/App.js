406f1a059d1b165833e564cc229fadaaed34bc59
DS Hwang <hds2g22@gmail.com>
2020-01-30 06:48:26 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,87 @@
 import React from "react";
-
-//function Food({ props.favorite }) {
-//function Food( props ) {
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture}></img>
-    </div>
-  );
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id: 1,
+    rating: 3.0,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
+    id: 2,
+    rating: 4.3,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
+    id: 3,
+    rating: 5.0,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
+    id: 4,
+    rating: 3.0,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
+    id: 5,
+    rating: 4.0,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
+//function Food({ props.favorite }) {
+//function Food( props ) {
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}></img>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Good morning!!</h1>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          rating={dish.rating}
+          name={dish.name}
+          picture={dish.image}
+        />
       ))}
     </div>
   );
 }
 
 export default App;
+
+// Arrow Function
+// (param1, param2, …, paramN) => { statements }
+// (param1, param2, …, paramN) => expression
+// 다음과 동일함:  => { return expression; }
+
+// 매개변수가 하나뿐인 경우 괄호는 선택사항:
+// (singleParam) => { statements }
+//singleParam => { statements }
+
+// 매개변수가 없는 함수는 괄호가 필요:
+// () => { statements }
